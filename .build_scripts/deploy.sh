daa2c93683d8b54ee343029bf008908bd3bec204
wani <wani420@gmail.com>
2016-09-27 21:41:15 +0800

    Update deploy.sh

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-# If this is the deploy branch, push it up to gh-pages
-echo "${DEPLOY_BRANCH}"
-echo "${GH_REF}"
-if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
-  echo "Get ready, we're pushing to gh-pages!"
-  rm -rf site
-  mkdir site
-  cp -a dist site
-  cp -a img fonts index.html style-rtl.css locale.js oauth.json CNAME site
-  cd site
-  git init
-  git config user.name "Travis-CI"
-  git config user.email "travis@somewhere.com"
-  git add .
-  git commit -m "CI deploy to gh-pages"
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
-else
-  echo "Not a publishable branch so we're all done here"
-fi
+echo "Get ready, we're pushing to gh-pages!"
+rm -rf site
+mkdir site
+cp -a dist site
+cp -a img fonts index.html style-rtl.css locale.js oauth.json CNAME site
+cd site
+git init
+git checkout --orphan gh-pages
+git add .
+git commit -m "Deploy to gh-pages"
+git remote add origin git@github.com:tytca/prose.git
+git push -f
+echo "Finish pushing to gh-pages!"
